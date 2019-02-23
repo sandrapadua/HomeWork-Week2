f05bb82be5d34d05ffb3c009f03ee6475635aebb
Sandra Padua <sandrapadua5@gmail.com>
2019-02-23 14:03:58 +0100

    Assignment 1: ShoppingCart

---
+++ src/ShoppingCart.js
@@ -0,0 +1,45 @@
+class ShoppingCart{
+    constructor() {
+        this.item = []
+      }
+
+    
+        addItem(itemName, quantity, price) {
+
+
+            const itemsList = {
+                name :itemName,
+                quantity : quantity,
+                pricePerUnit : price
+            }
+            return this.item.push(itemsList)
+
+        }
+            getItems() {
+                return this.item
+            }
+            clear(){
+                this.item =[]
+            } 
+            total(){
+
+ const priceOfEachItem = this.getItems().map((eachItem) => {
+return eachItem.quantity * eachItem.pricePerUnit
+})
+const totalPrice = priceOfEachItem.reduce((accumulator,currentValue) => {
+return accumulator+currentValue
+},0)
+
+return totalPrice   
+console.log(total())
+            } 
+
+}
+
+const cart = new ShoppingCart()
+
+
+
+
+
+module.exports = ShoppingCart
